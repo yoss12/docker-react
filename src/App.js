d57f6221ba01dfda96a35237ae08cc67ba1c1ca6
Yossi Shalem <yossis@selina.com>
2019-12-31 19:51:40 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save test1
         </p>
         <a
           className="App-link"
@@ -18,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <div>Test</div>
       </header>
     </div>
   );
